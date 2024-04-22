ef100e267741c907cd34cc173b6ad31d2de1ac21
Mircea-at-Stefanini <129756559+Mircea-at-Stefanini@users.noreply.github.com>
2024-04-22 16:00:48 +0300

    Add files via upload

---
+++ Calculate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeClassLibrary
+{
+    public class Calculate
+    {
+        public Calculate() { }
+
+        public double Add(double x, double y) { return x + y; }
+        public double Subtract(double x, double y) { return x - y; }
+    }
+}
