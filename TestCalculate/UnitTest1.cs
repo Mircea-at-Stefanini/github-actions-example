6177d50a1bbd7a47c4e4399d987b85792bc177e5
Mircea Puiu <Mircea.Puiu@stefanini.com>
2024-04-22 16:35:56 +0300

    Add test for Subtract and rename the test methods

--- TestCalculate/UnitTest1.cs
+++ TestCalculate/UnitTest1.cs
@@ -13,7 +13,7 @@ namespace TestCalculate
         }
 
         [Test]
-        public void Test1()
+        public void TestAdd()
         {
             var x = 10;
             var y = 5;
@@ -21,6 +21,17 @@ namespace TestCalculate
             var expected = 15;
 
             Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void TestSubtract()
+        {
+            var x = 10;
+            var y = 5;
+            var result = calculate.Subtract(x, y);
+            var expected = 5;
+
+            Assert.That (result, Is.EqualTo(expected));
         }
     }
 }
